7f1d0e90aaa19043513b26bea8cf432874320581
Lucas San Martin <lucassanmartin76@gmail.com>
2024-06-06 02:37:27 -0300

    6 de junio

--- lab-07/sim.c
+++ lab-07/sim.c
@@ -6,14 +6,14 @@
 #define SWAP 16
 #define MAX_PROCESOS 100
 
-int lru_index=0;
+int lru_index = 0;
 struct Pagina
 {
     int pid;
     int indice;
     int nroEnMemoria;
-    int memoria; // 1 es memoria ram, 2 es memoria secundaria
-    int lru_indice; //indice para el remplazo lru
+    int memoria;    // 1 es memoria ram, 2 es memoria secundaria
+    int lru_indice; // indice para el remplazo lru
 };
 typedef struct Pagina pagina_t;
 
@@ -38,39 +38,139 @@ struct RAM
 };
 typedef struct RAM ram_t;
 
-void usage(char *programa){
-    printf("Uso: %s -f | -l \n",programa);
+void usage(char *programa)
+{
+    printf("Uso: %s -f | -l \n", programa);
     exit(EXIT_FAILURE);
+}
+
+void imprimirTablaPaginacion(process_t procesos[])
+{
+    int j, k;
+    printf("\n");
+    printf("R indica memoria ram, S indica memoria secundaria. Se va a imprimir con el siguiente formato: memoria en la que esta.marco de la memoria\n");
+    for (j = 0; j < MAX_PROCESOS; j++)
+    {
+        if (procesos[j].activo == 1 && procesos[j].pid != -1)
+        {
+            printf("Proceso %d: ", procesos[j].pid);
+            for (k = 0; k < VIRTUAL; k++)
+            {
+                if (procesos[j].paginas[k].nroEnMemoria == -1)
+                {
+                    printf("- ");
+                }
+                else if (procesos[j].paginas[k].memoria == 1)
+                {
+                    printf("r.%d ", procesos[j].paginas[k].nroEnMemoria);
+                }
+                else if (procesos[j].paginas[k].memoria == 2)
+                {
+                    printf("s.%d ", procesos[j].paginas[k].nroEnMemoria);
+                }
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+void imprimirMemorias(ram_t ram, memSecundaria_t memSecundaria)
+{
+    int i, j;
+    printf("Las memorias se van a imprimir con el siguiente formato: Proceso.Pagina del proceso\n");
+    // Imprimo la memoria fisica
+    printf("Memoria fisica: ");
+    for (j = 0; j < FISICA; j++)
+    {
+        if (ram.paginas[j].pid == -1)
+        {
+            printf("- ");
+        }
+        else
+        {
+            printf("%d.%d ", ram.paginas[j].pid, ram.paginas[j].indice);
+        }
+    }
+    printf("\n");
+
+    // Imprimo la memoria secundaria
+    printf("Memoria secundaria: ");
+    for (i = 0; i < SWAP; i++)
+    {
+        if (memSecundaria.paginas[i].pid == -1)
+        {
+            printf("- ");
+        }
+        else
+        {
+            printf("%d.%d ", memSecundaria.paginas[i].pid, memSecundaria.paginas[i].indice);
+        }
+    }
+    printf("\n");
+}
+
+int gestionarProcesoEnMemoria(int modo, int pid, int pagina, process_t procesos[], ram_t *ram)
+{
+    if (modo == 'l' && procesos[pid - 1].paginas[pagina].memoria == 1)
+    {
+        printf("Actualizando lru_indice de pagina en RAM: Proceso %d, Pagina %d, lru_indice %d -> %d\n", pid, pagina, procesos[pid - 1].paginas[pagina].lru_indice, lru_index);
+        procesos[pid - 1].paginas[pagina].lru_indice = lru_index;
+        ram->paginas[procesos[pid - 1].paginas[pagina].nroEnMemoria].lru_indice = lru_index;
+        lru_index++;
+        return 0; // Indica que no se debe continuar con la siguiente iteración
+    }
+    else if (modo == 'f' && procesos[pid - 1].paginas[pagina].memoria == 1)
+    {
+        printf("La pagina:%d del Proceso: %d ya se encuentra en la memoria\n", pagina, pid);
+        return 0; // Indica que no se debe continuar con la siguiente iteración
+    }
+    return 1; // Indica que se debe continuar y se debe seguir con la lógica del programa
+}
+
+void inicializarProcesoSiEsNecesario(process_t procesos[], int pid)
+{
+    if (procesos[pid - 1].activo == -1)
+    {
+        procesos[pid - 1].activo = 1;
+        procesos[pid - 1].pid = pid;
+        for (int i = 0; i < VIRTUAL; i++)
+        {
+            procesos[pid - 1].paginas[i].pid = pid;
+            procesos[pid - 1].paginas[i].nroEnMemoria = -1;
+            procesos[pid - 1].paginas[i].indice = i;
+            procesos[pid - 1].paginas[i].lru_indice = -1;
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    if(argc!= 2){
+    if (argc != 2)
+    {
         usage(argv[0]);
-        }
-
-    char modo=argv[1][1];
-
+    }
+    char modo = argv[1][1];
     ram_t ram;
     memSecundaria_t memSecundaria;
- process_t procesos[MAX_PROCESOS];
+    process_t procesos[MAX_PROCESOS];
 
     int i;
     for (i = 0; i < MAX_PROCESOS; i++)
     {
-     	procesos[i].activo = -1;
+        procesos[i].activo = -1;
     }
 
     for (i = 0; i < FISICA; i++)
     {
-     	ram.paginas[i].pid = -1;
+        ram.paginas[i].pid = -1;
     }
 
     for (i = 0; i < SWAP; i++)
     {
-     	memSecundaria.paginas[i].pid = -1;
-  }
-    ram.fifo_index = 0; // Inicializa el índice FIFO
+        memSecundaria.paginas[i].pid = -1;
+    }
+    ram.fifo_index = 0;
     int encontroHueco;
     int pid;
     int pagina;
@@ -78,133 +178,90 @@ int main(int argc, char *argv[])
     int paginaRemplazo;
     while (scanf("%d\n%d", &pid, &pagina) != EOF)
     {
-     	encontroHueco=0;
-        paginaRemplazo=0;
-        if (procesos[pid - 1].activo == -1)
+        encontroHueco = 0;
+        paginaRemplazo = 0;
+        inicializarProcesoSiEsNecesario(procesos, pid);
+
+        if (gestionarProcesoEnMemoria(modo, pid, pagina, procesos, &ram) == 1)
         {
-            procesos[pid - 1].activo = 1;
-            procesos[pid - 1].pid = pid;
-            for (i = 0; i < VIRTUAL; i++)
+            // accedo a la ram
+            for (i = 0; i < FISICA; i++)
             {
-             	procesos[pid - 1].paginas[i].pid = pid;
-                procesos[pid - 1].paginas[i].nroEnMemoria = -1;
-                procesos[pid - 1].paginas[i].indice = i;
-                procesos[pid - 1].paginas[i].lru_indice = -1;
-            }
-	}
-
-    if(modo=='l' && procesos[pid - 1].activo==1){
-                procesos[pid - 1].paginas[pagina].lru_indice=lru_index;
-                lru_index++;
-
-    }else {
-
-
-
-
-	// accedo a la ram
-        for (i = 0; i < FISICA; i++)
-        {
-            if (ram.paginas[i].pid == -1)
-            {
-             	procesos[pid - 1].paginas[pagina].nroEnMemoria = i;
-                procesos[pid - 1].paginas[pagina].memoria = 1;
-                procesos[pid - 1].paginas[pagina].lru_indice=lru_index;
-                lru_index++;
-                ram.paginas[i] = procesos[pid - 1].paginas[pagina];
-                encontroHueco = 1;
-                break;
-            }
- }
-
-	if (encontroHueco == 0){
-            //accedo a la memoria secundaria
-            if(modo=='f'){
-                for (i = 0; i < SWAP; i++)
-                    {
-                    if (memSecundaria.paginas[i].pid == -1)
+                if (ram.paginas[i].pid == -1)
                 {
                     procesos[pid - 1].paginas[pagina].nroEnMemoria = i;
                     procesos[pid - 1].paginas[pagina].memoria = 1;
-                    procesos[pid - 1].paginas[pagina].lru_indice=lru_index;
+                    procesos[pid - 1].paginas[pagina].lru_indice = lru_index;
                     lru_index++;
-                    memSecundaria.paginas[i]=ram.paginas[ram.fifo_index];
+                    ram.paginas[i] = procesos[pid - 1].paginas[pagina];
+                    printf("Cargando en RAM: Proceso %d, Pagina %d en marco %d\n", pid, pagina, i);
+                    encontroHueco = 1;
                     break;
                 }
-            } 
-            //si esta todo lleno tiene que terminar el programa y no deberia hacer nada
-                ram.paginas[ram.fifo_index] = procesos[pid - 1].paginas[pagina];
-                ram.fifo_index = (ram.fifo_index + 1) % FISICA; // Avanza el índice FIFO circularmente
             }
-            if(modo=='l'){
-                    paginaRemplazoLru=ram.paginas[0].lru_indice;
-                for (i = 1; i < FISICA; i++){
-                    if(paginaRemplazoLru>ram.paginas[i].lru_indice){
-                    paginaRemplazo=i;
-                    }
-                      }
-                for (i = 0; i < SWAP; i++)
+            if (encontroHueco == 0)
+            {
+                // accedo a la memoria secundaria
+                if (modo == 'f')
+                {
+                    printf("No se encontró hueco en RAM, se va a utilizar FIFO\n");
+                    for (i = 0; i < SWAP; i++)
                     {
-                    if (memSecundaria.paginas[i].pid == -1)
+                        if (memSecundaria.paginas[i].pid == -1)
                         {
-                    procesos[pid - 1].paginas[pagina].nroEnMemoria = i;
-                    procesos[pid - 1].paginas[pagina].memoria = 1;
-                    procesos[pid - 1].paginas[pagina].lru_indice=lru_index;
-                    lru_index++;
-                    memSecundaria.paginas[i]=ram.paginas[paginaRemplazo];
-                    break;
+                            procesos[pid - 1].paginas[pagina].nroEnMemoria = ram.fifo_index;
+                            procesos[pid - 1].paginas[pagina].memoria = 1;
+                            procesos[pid - 1].paginas[pagina].lru_indice = lru_index;
+                            lru_index++;
+                            memSecundaria.paginas[i] = ram.paginas[ram.fifo_index];
+                            memSecundaria.paginas[i].memoria = 2;
+                            procesos[ram.paginas[ram.fifo_index].pid - 1].paginas[ram.paginas[ram.fifo_index].indice].memoria = 2;
+                            printf("Moviendo de RAM a MemSecundaria: Proceso %d, Pagina %d de marco %d a marco %d en swap\n", ram.paginas[ram.fifo_index].pid, ram.paginas[ram.fifo_index].indice, ram.fifo_index, i);
+                            break;
+                        }
                     }
-                ram.paginas[paginaRemplazo]=procesos[pid -1].paginas[pagina];
-                  
-	            }
-            }
-    }
-    }
-// Imprimo la tabla de paginacion de cada proceso
-    int j;
-    int k;
-    for (j = 0; j < MAX_PROCESOS; j++) {
-
-        if (procesos[j].activo == 1 && procesos[j].pid != -1) {
-            printf("Proceso %d: ", procesos[j].pid);
-
-            for (k = 0; k <VIRTUAL; k++) {
-                if (procesos[j].paginas[k].nroEnMemoria == -1) {
-                    printf("- ");
-                } else {
-                    printf("%d ", procesos[j].paginas[k].nroEnMemoria);
+                    // si esta todo lleno tiene que terminar el programa y no deberia hacer nada
+                    ram.paginas[ram.fifo_index] = procesos[pid - 1].paginas[pagina];
+                    printf("Cargando en RAM: Proceso %d, Pagina %d en marco %d\n", pid, pagina, ram.fifo_index);
+                    ram.fifo_index = (ram.fifo_index + 1) % FISICA; 
                 }
-             }
-	    printf("\n");
+                if (modo == 'l')
+                {
+                    printf("No se encontró hueco en RAM, se va a utilizar LRU\n");
+
+                    paginaRemplazo = 0;
+                    paginaRemplazoLru = ram.paginas[0].lru_indice;
+                    for (i = 1; i < FISICA; i++)
+                    {
+                        if (paginaRemplazoLru > ram.paginas[i].lru_indice)
+                        {
+                            paginaRemplazoLru = ram.paginas[i].lru_indice;
+                            paginaRemplazo = i;
+                        }
+                    }
+                    printf("Página a reemplazar: Proceso %d, Pagina %d, marco %d, lru_indice %d\n", ram.paginas[paginaRemplazo].pid, ram.paginas[paginaRemplazo].indice, paginaRemplazo, ram.paginas[paginaRemplazo].lru_indice);
+                    for (i = 0; i < SWAP; i++)
+                    {
+                        if (memSecundaria.paginas[i].pid == -1)
+                        {
+                            procesos[pid - 1].paginas[pagina].nroEnMemoria = paginaRemplazo;
+                            procesos[pid - 1].paginas[pagina].memoria = 1;
+                            procesos[pid - 1].paginas[pagina].lru_indice = lru_index;
+                            lru_index++;
+                            memSecundaria.paginas[i] = ram.paginas[paginaRemplazo];
+                            memSecundaria.paginas[i].memoria = 2;
+                            procesos[ram.paginas[paginaRemplazo].pid - 1].paginas[ram.paginas[paginaRemplazo].indice].memoria = 2;
+                            printf("Moviendo de RAM a MemSecundaria: Proceso %d, Pagina %d de marco %d a marco %d en swap, lru_indice %d\n", ram.paginas[paginaRemplazo].pid, ram.paginas[paginaRemplazo].indice, paginaRemplazo, i, ram.paginas[paginaRemplazo].lru_indice);
+                            break;
+                        }
+                    }
+                    ram.paginas[paginaRemplazo] = procesos[pid - 1].paginas[pagina];
+                    printf("Cargando en RAM: Proceso %d, Pagina %d en marco %d, lru_indice %d\n", pid, pagina, paginaRemplazo, lru_index - 1);
+                }
+            }
         }
     }
-
-    // Imprimo la memoria fisica
-    printf("Memoria fisica: ");
-    for (j = 0; j < FISICA; j++) {
-
-        if (ram.paginas[j].pid == -1) {
-            printf("- ");
-        } else {
-            printf("%d.%d ", ram.paginas[j].pid, ram.paginas[j].indice);
-        }
-    }
-    printf("\n");
-  // Imprimo la memoria secundaria
-    printf("Memoria secundaria: ");
-    for (i = 0; i < SWAP; i++) {
-
-        if (memSecundaria.paginas[i].pid == -1) {
-            printf("- ");
-        } else {
-            printf("%d.%d ", memSecundaria.paginas[i].pid, memSecundaria.paginas[i].indice);
-        }
-    }
-    printf("\n");
-
-
-
+    imprimirTablaPaginacion(procesos);
+    imprimirMemorias(ram, memSecundaria);
     exit(EXIT_SUCCESS);
-
 }
-    
